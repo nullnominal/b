5092da764eb9976031f269b6652239751c7fdfaa
rexim <reximkut@gmail.com>
2025-08-05 15:42:40 +0700

    bgen: print detected codegens

--- src/bgen.rs
+++ src/bgen.rs
@@ -20,13 +20,18 @@ pub unsafe fn main(mut _argc: i32, mut _argv: *mut*mut c_char) -> Option<()> {
     if !read_entire_dir(parent, &mut children) { return None; }
     qsort(children.items as *mut c_void, children.count, size_of::<*const c_char>(), compar_cstr);
     let mut sb: String_Builder = zeroed();
+    printf(c!("CODEGENS:\n"));
     sb_appendf(&mut sb, c!("codegens! {\n"));
     for i in 0..children.count {
         let child = *children.items.add(i);
         if *child == '.' as c_char { continue; }
         if strcmp(child, c!("mod.rs")) == 0 { continue; }
+        // TODO: skip the modules that have invalid Rust names.
+        //   Or is there any way to accomodate them into the Rust module system too?
+        //   In any case we should do something with the invalid module names.
         let child = temp_strip_suffix(child, c!(".rs")).unwrap_or(child);
         sb_appendf(&mut sb, c!("    %s,\n"), child);
+        printf(c!("    %s\n"), child);
     }
     sb_appendf(&mut sb, c!("}\n"));
     let output_path = temp_sprintf(c!("%s/.INDEX.rs"), parent);
